66aace7e996ae6a76e6498951834d59b21c5b2bb
Yamamoto Koki <koki.yamamoto@medley.jp>
2026-09-15 10:18:13 +0900

    style: describeでグルーピング

--- spec/template_processor_spec.rb
+++ spec/template_processor_spec.rb
@@ -54,76 +54,81 @@ RSpec.describe XlsxTemplater::TemplateProcessor do
   def dollar(key)
     "$#{key.to_s.upcase}$"
   end
-  
+
   def mustache(key)
     "{{#{key.to_s.upcase}}}"
   end
 
-  it 'ドル記号で囲まれたパラメータがスキャンされること' do
-    fixture = xlsx_with(build_shared_strings_xml(%w[$PATIENT_ID$ $PATIENT_NAME$]))
-    out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
-    expect(out).to eq(%w[PATIENT_ID PATIENT_NAME])
-  end
+  describe '.scan_params' do
+    it 'ドル記号で囲まれたパラメータがスキャンされること' do
+      fixture = xlsx_with(build_shared_strings_xml(%w[$PATIENT_ID$ $PATIENT_NAME$]))
+      out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
+      expect(out).to eq(%w[PATIENT_ID PATIENT_NAME])
+    end
 
-  it '二重波括弧で囲まれたパラメータがスキャンされること' do
-    fixture = xlsx_with(build_shared_strings_xml(%w[{{PATIENT_ID}} {{PATIENT_NAME}}]))
-    out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
-    expect(out).to eq(%w[PATIENT_ID PATIENT_NAME])
-  end
+    it '二重波括弧で囲まれたパラメータがスキャンされること' do
+      fixture = xlsx_with(build_shared_strings_xml(%w[{{PATIENT_ID}} {{PATIENT_NAME}}]))
+      out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
+      expect(out).to eq(%w[PATIENT_ID PATIENT_NAME])
+    end
 
-  it 'ドル記号と二重波括弧の両方で囲まれたパラメータがスキャンされること' do
-    fixture = xlsx_with(build_shared_strings_xml(%w[$PATIENT_ID$ {{PATIENT_NAME}}]))
-    out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
-    expect(out).to eq(%w[PATIENT_ID PATIENT_NAME])
-  end
+    it 'ドル記号と二重波括弧の両方で囲まれたパラメータがスキャンされること' do
+      fixture = xlsx_with(build_shared_strings_xml(%w[$PATIENT_ID$ {{PATIENT_NAME}}]))
+      out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
+      expect(out).to eq(%w[PATIENT_ID PATIENT_NAME])
+    end
 
-  it 'キーを含まない文字列しかないときに[]を返すか' do
-    fixture = xlsx_with(build_shared_strings_xml(['no keys']))
-    out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
-    expect(out).to eq([])
-  end
-
-  it 'ドル記号のキーが値に置き換わること' do
-    xml = build_shared_strings_xml(%w[$PATIENT_ID$ $PATIENT_NAME$])
-    out = parser.render(xml)
-    expect(out).to include(data[:patient_id])
-    expect(out).to include(data[:patient_name])
-    expect(out).not_to include('$PATIENT_ID$')
-    expect(out).not_to include('$PATIENT_NAME$')
-  end
-
-  it '二重波括弧のキーが値に置き換わること' do
-    xml = build_shared_strings_xml(%w[{{PATIENT_ID}} {{PATIENT_NAME}}])
-    out = parser.render(xml)
-    expect(out).to include(data[:patient_id])
-    expect(out).to include(data[:patient_name])
-    expect(out).not_to include('{{PATIENT_ID}}')
-    expect(out).not_to include('{{PATIENT_NAME}}')
-  end
-
-  it 'ドル記号と二重波括弧の両方のキーが値に置き換わること' do
-    xml = build_shared_strings_xml(%w[$PATIENT_ID$ {{PATIENT_NAME}}])
-    out = parser.render(xml)
-    expect(out).to include(data[:patient_id])
-    expect(out).to include(data[:patient_name])
-    expect(out).not_to include('$PATIENT_ID$')
-    expect(out).not_to include('{{PATIENT_NAME}}')
-  end
-
-  it '全キーが値に置き換わること' do
-    xml = build_shared_strings_xml(data.keys.map { |key| dollar(key) })
-    out = parser.render(xml)
-    data.each do |key, value|
-      expect(out).to include(value.to_s)
-      expect(out).not_to include(dollar(key))
+    it 'キーを含まない文字列しかないときに[]を返すか' do
+      fixture = xlsx_with(build_shared_strings_xml(['no keys']))
+      out = XlsxTemplater::TemplateProcessor.scan_params(fixture.path)
+      expect(out).to eq([])
     end
   end
 
-  it '値に半角の & が含まれても壊れた XML を出力しないこと' do
-    data[:clinic_name] = 'メディカル&ケアクリニック'
-    xml = build_shared_strings_xml(['$CLINIC_NAME$'])
-    out = parser.render(xml)
-    expect(out).to include('メディカル&amp;ケアクリニック')
+  describe '#render' do
+    it 'ドル記号のキーが値に置き換わること' do
+      xml = build_shared_strings_xml(%w[$PATIENT_ID$ $PATIENT_NAME$])
+      out = parser.render(xml)
+      expect(out).to include(data[:patient_id])
+      expect(out).to include(data[:patient_name])
+      expect(out).not_to include('$PATIENT_ID$')
+      expect(out).not_to include('$PATIENT_NAME$')
+    end
+
+    it '二重波括弧のキーが値に置き換わること' do
+      xml = build_shared_strings_xml(%w[{{PATIENT_ID}} {{PATIENT_NAME}}])
+      out = parser.render(xml)
+      expect(out).to include(data[:patient_id])
+      expect(out).to include(data[:patient_name])
+      expect(out).not_to include('{{PATIENT_ID}}')
+      expect(out).not_to include('{{PATIENT_NAME}}')
+    end
+
+    it 'ドル記号と二重波括弧の両方のキーが値に置き換わること' do
+      xml = build_shared_strings_xml(%w[$PATIENT_ID$ {{PATIENT_NAME}}])
+      out = parser.render(xml)
+      expect(out).to include(data[:patient_id])
+      expect(out).to include(data[:patient_name])
+      expect(out).not_to include('$PATIENT_ID$')
+      expect(out).not_to include('{{PATIENT_NAME}}')
+    end
+
+    it '全キーが値に置き換わること' do
+      xml = build_shared_strings_xml(data.keys.map { |key| dollar(key) })
+      out = parser.render(xml)
+      data.each do |key, value|
+        expect(out).to include(value.to_s)
+        expect(out).not_to include(dollar(key))
+      end
+    end
+
+    it '値に半角の & が含まれても壊れた XML を出力しないこと' do
+      data[:clinic_name] = 'メディカル&ケアクリニック'
+      xml = build_shared_strings_xml(['$CLINIC_NAME$'])
+      out = parser.render(xml)
+      expect(out).to include('メディカル&amp;ケアクリニック')
+    end
+
   end
 end
 
